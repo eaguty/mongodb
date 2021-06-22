17a5ac6583228fde74893bb113d4ea7d3bd22ba2
eaguty <adriangutierrezpabon@gmail.com>
2021-06-21 20:06:36 -0500

    Add files via web
    Primer Commit

---
+++ borrado/src/main/java/borrado/MongoDBApp.java
@@ -0,0 +1,25 @@
+package borrado;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+
+public class MongoDBApp {
+
+	public static void main(String[] args) {
+
+		try {
+			MongoClient mongoClient = new MongoClient();
+			
+			//DBCursor results = 
+			
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
