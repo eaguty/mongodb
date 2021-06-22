b8bb342bcc4480c3784026d72de757c78b807d83
Adrian Gutierrez <adriangutierrezpabon@gmail.com>
2021-06-22 18:50:18 -0500

    Proyecto Spring Data with Mongo DB

---
+++ src/main/java/com/tvazteca/Borrado/BorradoApplication.java
@@ -0,0 +1,13 @@
+package com.tvazteca.Borrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvazteca/Borrado/BorradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvazteca.Borrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
